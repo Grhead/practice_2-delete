a28ec6ddb82677b0dd16e5299ea79221b6bca335
public <public@copp.local>
2023-05-17 09:59:23 +0700

    add function 4

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
+import "time"
+import "math/rand"
 
+func main() {
+	fmt.Println(ForBranch1(4, 5))
+	fmt.Println(ForBranch2(4.5))
+	fmt.Println(ForBranch3())
+	fmt.Println(ForBranch4("first", "second", "third"))
+}
+
+func ForBranch1(a int, b int) int {
+	return a ^ b - a*2
+}
+
+func ForBranch2(a float32) float32 {
+	return a / float32(time.Now().Hour())
+}
+
+func ForBranch3() string {
+	return fmt.Sprintf("%f", rand.Float64())
+}
+
+func ForBranch4(a string, b string, c string) (result []string) {
+	result = append(result, a)
+	result = append(result, b)
+	result = append(result, c)
+	return result
 }
