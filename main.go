b5a43f20fe8af9f28f3a3b4e02a3ec271c53354c
public <public@copp.local>
2023-05-17 09:37:38 +0700

    add function 1

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println(ForBranch1(4, 5))
+}
+
+func ForBranch1(a int, b int) int {
+	return a ^ b - a*2
+}
+
+func ForBranch2() float32 {
+
+}
+func ForBranch3() string {
+
+}
+func ForBranch4() []string {
 
 }
