dd4c039f6615d09bd07243b755f71fe795e5cc8f
public <public@copp.local>
2023-05-17 10:04:05 +0700

    modify

--- main.go
+++ main.go
@@ -2,28 +2,12 @@ package main
 
 import "fmt"
 import "time"
-import "math/rand"
 
 func main() {
-	fmt.Println(ForBranch1(4, 5))
 	fmt.Println(ForBranch2(4.5))
-}
-
-func ForBranch1(a int, b int) int {
-	return a ^ b - a*2
 }
 
 func ForBranch2(a float32) float32 {
 	return a / float32(time.Now().Hour())
 }
 
-func ForBranch3() string {
-	return fmt.Sprintf("%f", rand.Float64())
-}
-
-
-
-
-func ForBranch4() []string {
-
-}
