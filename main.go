caac93078a20d88640f5930f37d0062db3095ae0
public <public@copp.local>
2023-05-17 10:05:29 +0700

    modify

--- main.go
+++ main.go
@@ -4,12 +4,7 @@ import "fmt"
 import "time"
 
 func main() {
-	fmt.Println(ForBranch1(4, 5))
 	fmt.Println(ForBranch2(4.5))
-}
-
-func ForBranch1(a int, b int) int {
-	return a ^ b - a*2
 }
 
 func ForBranch2(a float32) float32 {
