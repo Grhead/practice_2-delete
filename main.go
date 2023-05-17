2df2cf9d296ffb9e5048f62e895f3a1678c536c9
public <public@copp.local>
2023-05-17 09:41:23 +0700

    add function 2

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import "fmt"
+import "time"
+
 func main() {
+	fmt.Println(ForBranch1(4, 5))
+	fmt.Println(ForBranch2(4.5))
+}
+
+func ForBranch1(a int, b int) int {
+	return a ^ b - a*2
+}
+
+func ForBranch2(a float32) float32 {
+	return a / float32(time.Now().Hour())
+}
+
+
+
+
+
+
+
+
+func ForBranch3() string {
+
+}
+func ForBranch4() []string {
 
 }
